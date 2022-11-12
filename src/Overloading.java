ddbb8e35cae83ab409aa3fed0a9ab368a68b566e
Darshan Patil <iampatildarsh>
2022-11-12 21:54:34 +0530

    new file added

---
+++ src/Overloading.java
@@ -0,0 +1,15 @@
+public class Overloading {
+    void show(int a, String b)
+    {
+        System.out.println("Hello");
+    }
+    void show(int a) // same method , different arguments
+    {
+        System.out.println("Namaste");
+    }
+
+    public static void main(String[] args) {
+        Overloading O = new Overloading(); //create object
+        O.show(10,"Darshan");
+    }
+}
